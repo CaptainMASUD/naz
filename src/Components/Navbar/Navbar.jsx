6ce4f8f8ae8b6e707168890616e16a172ec2eac0
masudul alam <masudulalam972@gmail.com>
2024-12-09 00:27:25 +0600

    --udpdates in bound

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { FaEnvelope, FaPhone, FaHome, FaCogs, FaInfoCircle, FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import {
+  FaEnvelope,
+  FaPhone,
+  FaHome,
+  FaCogs,
+  FaInfoCircle,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
 import { RiContactsLine } from "react-icons/ri";
-import logo from "../../logo/N.png"
+import logo from "../../logo/N.png";
 const NavbarComponent = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -15,7 +23,9 @@ const NavbarComponent = () => {
       to={to}
       className={({ isActive }) =>
         `flex items-center px-3 py-2 rounded-md text-sm font-medium ${
-          isActive ? 'text-[#BE3144] bg-gray-900' : 'text-white hover:text-[#BE3144] hover:bg-gray-700'
+          isActive
+            ? "text-[#BE3144] bg-gray-900"
+            : "text-white hover:text-[#BE3144] hover:bg-gray-700"
         }`
       }
     >
@@ -30,34 +40,50 @@ const NavbarComponent = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0 flex items-center">
-              <img src={logo} className="h-8 w-8 rounded-full mr-2" alt="NAJ Logo" />
-              <span className="text-white text-xl font-semibold" style={{fontFamily : "Playwrite HR Lijeva"}}>Naj International </span>
+              <img
+                src={logo}
+                className="h-8 w-8 rounded-full mr-2"
+                alt="NAJ Logo"
+              />
+              <span
+                className="text-white text-xl font-semibold"
+                style={{ fontFamily: "Playwrite HR Lijeva" }}
+              >
+                Naj International{" "}
+              </span>
             </Link>
           </div>
-         
+
           <div className="hidden md:flex items-center mt-16 justify-center flex-1">
             <div className="flex items-baseline space-x-4">
-              <NavItem to="/" icon={FaHome}>Home</NavItem>
-              <NavItem to="/services" icon={FaCogs}>Services</NavItem>
-              <NavItem to="/about" icon={FaInfoCircle}>About</NavItem>
-              <NavItem to="/contact" icon={RiContactsLine}>Contact us</NavItem>
+              <NavItem to="/" icon={FaHome}>
+                Home
+              </NavItem>
+              <NavItem to="/services" icon={FaCogs}>
+                Services
+              </NavItem>
+              <NavItem to="/about" icon={FaInfoCircle}>
+                About
+              </NavItem>
+              <NavItem to="/contact" icon={RiContactsLine}>
+                Contact us
+              </NavItem>
             </div>
           </div>
           <div className="hidden md:flex items-center">
             <div className="flex items-center space-x-4 text-white">
-            <div className="flex items-center hover:underline hover:text-[#e64e63] duration-500 cursor-pointer">
-  <FaEnvelope className="mr-2 mt-1" />
-  <a href="mailto:info@naj.com" className="hover:underline">
-    <span>info@naj.com</span>
-  </a>
-</div>
-<div className="flex items-center hover:underline hover:text-[#e64e63] duration-500 cursor-pointer">
-  <FaPhone className="mr-1 mt-1" />
-  <a href="tel:+8801744416607" className="hover:underline">
-    <span>+880 1744 416607</span>
-  </a>
-</div>
-
+              <div className="flex items-center hover:underline hover:text-[#e64e63] duration-500 cursor-pointer">
+                <FaEnvelope className="mr-2 mt-1" />
+                <a href="mailto:info@naj.com" className="hover:underline">
+                  <span>info@naj.com</span>
+                </a>
+              </div>
+              <div className="flex items-center hover:underline hover:text-[#e64e63] duration-500 cursor-pointer">
+                <FaPhone className="mr-1 mt-1" />
+                <a href="tel:+8801744416607" className="hover:underline">
+                  <span>+880 1744 416607</span>
+                </a>
+              </div>
             </div>
           </div>
           <div className="md:hidden flex items-center">
@@ -79,25 +105,47 @@ const NavbarComponent = () => {
       {isMenuOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <NavItem to="/" icon={FaHome}>Home</NavItem>
-            <NavItem to="/services" icon={FaCogs}>Services</NavItem>
-            <NavItem to="/about" icon={FaInfoCircle}>About</NavItem>
-            <NavItem to="/contact" icon={RiContactsLine}>Contact us</NavItem>
+            <NavItem to="/" icon={FaHome}>
+              Home
+            </NavItem>
+            <NavItem to="/services" icon={FaCogs}>
+              Services
+            </NavItem>
+            <NavItem to="/about" icon={FaInfoCircle}>
+              About
+            </NavItem>
+            <NavItem to="/contact" icon={RiContactsLine}>
+              Contact us
+            </NavItem>
           </div>
           <div className="pt-4 pb-3 border-t border-gray-700">
             <div className="flex items-center px-5">
               <div className="flex-shrink-0">
-                <img src={logo} className="h-10 w-10 rounded-full" alt="NAJ Logo" />
+                <img
+                  src={logo}
+                  className="h-10 w-10 rounded-full"
+                  alt="NAJ Logo"
+                />
               </div>
               <div className="ml-3">
-                <div className="text-base font-medium leading-none text-white">NAJ</div>
-                <div className="text-sm font-medium leading-none text-gray-400">info@NAJ.com</div>
+                <div className="text-base font-medium leading-none text-white">
+                  NAJ
+                </div>
+                <div className="text-sm font-medium leading-none text-gray-400">
+                  {" "}
+                  <a href="mailto:info@naj.com" className="hover:underline">
+                    <span>info@naj.com</span>
+                  </a>
+                </div>
               </div>
             </div>
             <div className="mt-3 px-2 space-y-1">
-              <a href="tel:+12345678900" className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
+              <a
+                href="tel:+8801744416607"
+                className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+              >
                 <FaPhone className="inline-block mr-2" />
-                +1 234 567 8900
+                +8801744416607
               </a>
             </div>
           </div>
@@ -108,4 +156,3 @@ const NavbarComponent = () => {
 };
 
 export default NavbarComponent;
-
